8e5e876bf81a49852f0737e0c49137d12a7c9054
PTSoup <phaimani.souphaphone@gmail.com>
2018-12-29 15:06:50 -0600

    Working on password authentication

--- app/models/user.js
+++ app/models/user.js
@@ -4,7 +4,25 @@ var Promise = require('bluebird');
 
 
 
+
+
 var User = db.Model.extend({
+  tableName: 'username',
+  initialize: function() {
+    this.on('creating', this.hashPassword, this);
+  },
+  hashPassword: function(model, attrs, options) {
+    return new Promise(function(resolve, reject) {
+      bcrypt.hash(model.attributes.password, 10, function(err, hash) {
+        if (err) {
+          reject(err);
+        } else {
+          model.set('password', hash);
+          resolve(hash);
+        }
+      });
+    });
+  }
 });
 
 module.exports = User;
